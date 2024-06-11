def1bddd8036d3248c4e865af8482db3a26e0641
SantiagoCodi <santiagodarderblanco@gmail.com>
2024-06-11 20:23:07 -0300

    Add files via upload

---
+++ Proyecto2/mayormenor.py
@@ -0,0 +1,29 @@
+def mayor(a,b):
+    """funcion que entre dos valor enteros devuelve el mayor
+
+    Args:
+        a (int): valor ingresado 1
+        b (int): valor ingresado 2
+
+    Returns:
+        valor mayor: despues de un condicional se define el mayor y se devuelve
+    """
+    if a > b:
+        return a
+    else:
+        return b
+
+def menor(a,b):
+    """funcion que define el menor de dos valor enteros
+
+    Args:
+        a (int): valor ingresado 1
+        b (int): valor ingresado 2
+
+    Returns:
+        valor menor: despues de un condicional se define el menor y se devuelve
+    """
+    if a > b:
+        return b
+    else:
+        return a
---
+++ Proyecto2/principal.py
@@ -0,0 +1,7 @@
+import mayormenor
+
+c=int(input('Ingrese un valor entero: '))
+d=int(input('Ingrese otro valor entero: '))
+res=mayormenor.mayor(c,d)
+print('****************************************')
+print('El numero mayor ingresado es: ',res)
